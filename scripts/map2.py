ab09d661d2c163db970ffc3848baeb4d3a8f2efa
Bradyi <ian.brady2@mail.dcu.ie>
2017-04-20 20:50:19 +0100

    Add files via upload
    Takes in population (some very small countries incorrect)
    Updated how edges are formed - takes into account population as previously described

--- scripts/map2.py
+++ scripts/map2.py
@@ -8,23 +8,22 @@ import numpy as np
 filename = 'Data.csv'
 
 G = nx.Graph()
-countries, lats, lons = [], [], []
+countries, lats, lons, pops = [], [], [], []
 pos = {}
 
 # Make this plot larger.
-plt.figure(figsize=(15,10))
+plt.figure(figsize=(15,9))
  
 with open(filename) as f:
     # Create a csv reader object.
     reader = csv.reader(f)
     
     # Store the latitudes and longitudes in the appropriate lists.
-    i = 0
     for row in reader:
         countries.append(row[0])
         lats.append(float(row[1]))
         lons.append(float(row[2]))
-        i = i + 1
+        pops.append(float(row[3]))
     
 m = Basemap(projection='robin', resolution = 'l', area_thresh = 1000.0,
               lat_0=0, lon_0=0)
@@ -44,10 +43,15 @@ for country, lon, lat, in zip(countries, lons, lats):
     
 #Calculate Distances between countries and add edges
 i = 1
-for c1, lon1, lat1, in zip(countries, lons, lats):
-    for c2, lon2, lat2, in zip(countries[i:], lons[i:], lats[i:]):
+for c1, lon1, lat1, pop1 in zip(countries, lons, lats, pops):
+    for c2, lon2, lat2, pop2  in zip(countries[i:], lons[i:], lats[i:], pops[i:]):
         dist = np.sqrt((lat2-lat1)**2 + (lon2-lon1)**2)
-        if dist < 10:
+        maxPop = max(pop1, pop2)
+        score  = np.power(maxPop,(1/4))/dist
+        print(dist)
+        print(maxPop)
+        print(score)
+        if score > 1:
             G.add_edge(c1, c2, weight = dist)
     i = i + 1
     
